5aed5a12a8e0856d1ac3793e8968949f0087a1e4
Nellie <80473940+nelliecrocker@users.noreply.github.com>
2021-09-14 20:34:45 -0400

    first push

---
+++ src/components/Auth.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+    render() {
+        return (
+            <div>
+                <p>"Sign up here"</p>
+            </div>
+        );
+    }
+}
+
+export default Auth;
---
+++ src/components/Landing.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Auth from './Auth'
+
+class Landing extends Component {
+
+    render() {
+        return (
+            <div>
+                <Auth />
+            </div>
+        )
+    }
+
+}
+
+export default Landing
